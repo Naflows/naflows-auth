2d924c0fbc98bacd48182b6a40f99d14278cd2dc
naflouille <Eadjjawn@gmail.com>
2025-06-16 09:40:52 +0200

    Removed dist files

--- software/dist/dir.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.software = void 0;
-var switch_server_reply_1 = require("./methods/switch-server-reply");
-exports.software = {
-    methods: {
-        manageErrorCode: switch_server_reply_1.SwitchServerReply
-    }
-};
