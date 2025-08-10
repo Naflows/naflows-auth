3ec18df31fa3a6f467172536e3b3ca88ea03edc3
naflouille <eadjjawn@gmail.com>
2025-08-10 17:58:33 +0200

    Hotfix: missing a line in run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ if [ "$TEST_PARAMETER" = "no-test" ]; then
     COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express" docker compose up -d
 else
     if [ "$TEST_PARAMETER" = "global" ]; then
+        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-global" docker compose down -v
         COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-global" docker compose up -d
     else
         echo "Unknown test parameter. Please use 'no-test' or 'run-test'."
