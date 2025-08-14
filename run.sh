a008b098b0006b49da5dfca97663bf4ee7e85798
naflouille <eadjjawn@gmail.com>
2025-08-14 12:47:07 +0200

    Added base of the dummy api

--- run.sh
+++ run.sh
@@ -4,19 +4,19 @@ trap 'echo "Error occurred. Press enter to continue."; read' ERR
 
 TEST_PARAMETER=$1
 
-COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-contracts,test-global" docker compose down -v
+COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-contracts,test-global,dummy-api" docker compose down -v
 
 if [ "$TEST_PARAMETER" = "no-test" ]; then
-    COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express" docker compose up -d
-    
+    COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,dummy-api" docker compose up -d
+
 else
     if [ "$TEST_PARAMETER" = "global" ]; then
-        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-global" docker compose up -d
+        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-global,dummy-api" docker compose up -d
     elif [ "$TEST_PARAMETER" = "contracts" ]; then
-        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-contracts" docker compose up -d
+        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-contracts,dummy-api" docker compose up -d
     elif [ "$TEST_PARAMETER" = "all" ]; then
-    
-        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-contracts,test-global" docker compose up -d
+
+        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-contracts,test-global,dummy-api" docker compose up -d
     else
         echo "Unknown test parameter. Please use 'no-test' or 'run-test'."
         bash
