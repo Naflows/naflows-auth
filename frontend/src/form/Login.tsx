58a3c8e79dedfed00cbd279edbca7e7d9563b88e
naflouille <eadjjawn@gmail.com>
2025-08-31 16:54:58 +0200

    Redesigner the register / login portal

--- frontend/src/form/Login.tsx
+++ frontend/src/form/Login.tsx
@@ -1,36 +1,45 @@
+import { useState } from "react";
+import Input from "../global/components/Input";
+
+
 const LoginForm = () => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <>
       <div className="inputs-container two-columns">
-        <div className="inputs-container two-rows">
-          <label className="text-size-20">Customer ID</label>
-          <input
-            className="text-size-20"
-            type={"text"}
-            placeholder="Enter your customer ID"
+        <div className="inputs-container two-rows global__input">
+          <Input
+            label="Customer ID"
+            type="text"
+            name="customerID"
+            required
+            maxLength={100}
           />
         </div>
-        <div className="inputs-container two-rows">
-          <label className="text-size-20">Identifier</label>
-          <input
-            className="text-size-20"
-            type={"text"}
-            placeholder="Enter your identifier"
+        <div className="inputs-container two-rows global__input">
+          <Input
+            label="Identifier"
+            type="number"
+            name="identifier"
+            required
+            maxLength={9}
           />
         </div>
       </div>
-      <div className="inputs-container">
-        <label className="text-size-20">Password</label>
-        <input
-          className="text-size-20"
-          type={"password"}
-          placeholder="Enter your password"
+      <div className="inputs-container global__input">
+        <Input
+          label="Password"
+          type="password"
+          name="password"
+          required
+          maxLength={100}
         />
       </div>
       <button
         className="primary-button text-size-20"
         onClick={async () => {
-          // Using fetch
+          setLoading(true);
           const response = await fetch("http://localhost:3000/client/login", {
             method: "POST",
             headers: {
@@ -39,13 +48,22 @@ const LoginForm = () => {
             body: JSON.stringify({
               identifier: "123456789",
               password: "W8JdVoy30xEa1hZ5aDVQ",
-              userID:"1"
+              userID: "1",
             }),
           });
           console.log(response);
+          setLoading(false);
         }}
       >
-        Log in
+        <span style={{
+          display: loading ? "none" : "block"
+        }}>Log in</span>
+
+        <div className="naflows__button__loader" style={{
+          display: loading ? "block" : "none"
+        }}>
+          <div className="naflows__button__loader__content"></div>
+        </div>
       </button>
     </>
   );
