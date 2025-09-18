600dde57af26dccf1f9153049751778b98a6a79d
naflouille <eadjjawn@gmail.com>
2025-09-18 16:55:30 +0200

    Refined UI via coloring.

--- run.sh
+++ run.sh
@@ -8,21 +8,21 @@ if [ "$RESET_ENVIRONMENT" = "true" ]; then
     rm -rf ./backend/auth-data
     docker volume rm $(docker volume ls -q)
     docker system prune -a -f --volumes
-    COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-services,test-global,dummy-api" docker compose down -v
+    COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-services,test-global,dummy-api,frontend" docker compose down -v
 else
-    COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-services,test-global,dummy-api" docker compose down
+    COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-services,test-global,dummy-api,frontend" docker compose down
 fi
 
 if [ "$TEST_PARAMETER" = "no-test" ]; then
-    COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,dummy-api" docker-compose up --build -d
+    COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,dummy-api,frontend" docker-compose up --build -d
 
 else
     if [ "$TEST_PARAMETER" = "global" ]; then
-        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-global,dummy-api" docker-compose up --build -d
+        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-global,dummy-api,frontend" docker-compose up --build -d
     elif [ "$TEST_PARAMETER" = "contracts" ]; then
-        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-services,dummy-api" docker-compose up --build -d
+        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-services,dummy-api,frontend" docker-compose up --build -d
     elif [ "$TEST_PARAMETER" = "all" ]; then
-        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-services,test-global,dummy-api" docker-compose up --build -d
+        COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express,test-services,test-global,dummy-api,frontend" docker-compose up --build -d
     else
         echo "Unknown test parameter. Please use 'no-test' or 'run-test'."
         bash
