bf88cd8f532f35bcc2da1ecaa2b97800683095d5
naflouille <eadjjawn@gmail.com>
2025-10-07 09:54:18 +0200

    Improved ui

---
+++ frontend/src/pages/services/manage/pages/overview/components/users/components/rights.tsx
@@ -0,0 +1,78 @@
+import type { ServiceRights } from "../../../../../../../../types/TunnelingTypes"
+import { createdAtToAgo } from "../../../../../../../account/sub-components/notifications/methods/createdAtToAgo"
+
+const hslToRgba = (h: number, s: number, l: number, a: number) => {
+    s /= 100;
+    l /= 100;
+    const k = (n: number) => (n + h / 30) % 12;
+    const a_ = s * Math.min(l, 1 - l);
+    const f = (n: number) => l - a_ * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+    return `rgba(${Math.round(f(0) * 255)}, ${Math.round(f(8) * 255)}, ${Math.round(f(4) * 255)}, ${a})`;
+}
+
+const ServiceRightsComponent = ({
+    rights, rightsDetailsLimit
+    
+}: {
+    rights: ServiceRights[],
+    rightsDetailsLimit: number,
+}) => {
+    return (
+        rights.map((right: ServiceRights,i) => {
+            return (
+                <div key={right.id} className="right__card" style={{
+                    animationDelay: `${i * 100}ms`
+                }}>
+                    <div className="right__card__body">
+                        <div className="right__header">
+                            <div className="right__color" style={{
+                                backgroundColor: hslToRgba(parseInt(right.hue), 70, 50, 1)
+                            }}></div>
+                            <div className="right__info">
+                                <span className="last__updated">Last updated {createdAtToAgo(right.updated_at)}</span>
+                                <h3>{right.name}</h3>
+                                <div className="rights__details">
+                                    {
+                                        right.rights.map((r, i) => {
+                                            if (i < rightsDetailsLimit) {
+                                                return (<span key={r} className="right__tag">{r}</span>)
+                                            } else if (i === rightsDetailsLimit) {
+                                                return (<span key={r} className="right__tag">+{right.rights.length - rightsDetailsLimit} more</span>)
+                                            }
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                        <div className="right__users">
+                            {
+                                right.usersPerRights && right.usersPerRights.length > 0 ? (
+                                    right.usersPerRights.map((user, i) => {
+                                        if (i < 3) {
+                                            return (
+                                                <div className="user__image__container" key={user.id}>
+                                                    <img src={user.profile_picture || ""} alt={user.username} />
+                                                </div>
+                                            )
+                                        } else if (i === 3) {
+                                            return (
+                                                <div className="user__image__container more__users" key={user.id}>
+                                                    +{right.usersPerRights!.length - 3}
+                                                </div>
+                                            )
+                                        }
+                                    })
+                                ) : (
+                                    <p>No users found for this right.</p>
+                                )
+                            }
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    )
+
+}
+
+export default ServiceRightsComponent;
--- frontend/src/pages/services/manage/pages/overview/components/users/index.tsx
+++ frontend/src/pages/services/manage/pages/overview/components/users/index.tsx
@@ -2,17 +2,11 @@ import { useEffect, useState } from "react";
 import type { ServicesForUserProps } from "../../../../../../../types/ServicesForUserProps"
 import axios from "axios";
 import type { ServiceRights } from "../../../../../../../types/TunnelingTypes";
-import { createdAtToAgo } from "../../../../../../account/sub-components/notifications/methods/createdAtToAgo";
 import '../../../../../../../../public/root/pages/services/manage/sub-components/Rights.scss';
+import Loader from "../../../../../../../global/components/Loader";
+import ServiceRightsComponent from "./components/rights";
 
-const hslToRgba = (h: number, s: number, l: number, a: number) => {
-    s /= 100;
-    l /= 100;
-    const k = (n: number) => (n + h / 30) % 12;
-    const a_ = s * Math.min(l, 1 - l);
-    const f = (n: number) => l - a_ * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
-    return `rgba(${Math.round(f(0) * 255)}, ${Math.round(f(8) * 255)}, ${Math.round(f(4) * 255)}, ${a})`;
-}
+
 
 const ServiceUsers = ({
     service
@@ -22,6 +16,7 @@ const ServiceUsers = ({
 
     const [rights, setRights] = useState<ServiceRights[]>([]);
     const [rightsDetailsLimit, setRightsDetailsLimit] = useState(5);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         async function fetchRights() {
@@ -34,6 +29,7 @@ const ServiceUsers = ({
                 }).then((response) => {
                     console.log("Fetched rights:", response.data.data);
                     setRights(response.data.data.rights);
+                    setIsLoading(false);
                 });
             } catch (error) {
                 console.error("Error fetching rights:", error);
@@ -46,63 +42,23 @@ const ServiceUsers = ({
     return (
         <div>
 
-            <div className="rights__container">
+            <div className="users__container">
                 {
-                    rights.length === 0 ? (
-                        <p>No rights sets found for this service.</p>
-                    ) : (
-                        rights.map((right: ServiceRights) => {
-                            return (
-                                <div key={right.id} className="right__card">
-                                    <div className="right__card__body">
-                                        <div className="right__header">
-                                            <div className="right__color" style={{
-                                                backgroundColor: hslToRgba(parseInt(right.hue), 70, 50, 1)
-                                            }}></div>
-                                            <div className="right__info">
-                                                <span className="last__updated">Last updated {createdAtToAgo(right.updated_at)}</span>
-                                                <h3>{right.name}</h3>
-                                                <div className="rights__details">
-                                                    {
-                                                        right.rights.map((r, i) => {
-                                                            if (i < rightsDetailsLimit) {
-                                                                return (<span key={r} className="right__tag">{r}</span>)
-                                                            } else if (i === rightsDetailsLimit) {
-                                                                return (<span key={r} className="right__tag">+{right.rights.length - rightsDetailsLimit} more</span>)
-                                                            }
-                                                        })
-                                                    }
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="right__users">
-                                            {
-                                                right.usersPerRights && right.usersPerRights.length > 0 ? (
-                                                    right.usersPerRights.map((user, i) => {
-                                                        if (i < 3) {
-                                                            return (
-                                                                <div className="user__image__container" key={user.id}>
-                                                                    <img src={user.profile_picture || ""} alt={user.username} />
-                                                                </div>
-                                                            )
-                                                        } else if (i === 3) {
-                                                            return (
-                                                                <div className="user__image__container more__users" key={user.id}>
-                                                                    +{right.usersPerRights!.length - 3}
-                                                                </div>
-                                                            )
-                                                        }
-                                                    })
-                                                ) : (
-                                                    <p>No users found for this right.</p>
-                                                )
-                                            }
-                                        </div>
-                                    </div>
+                    isLoading ? (
+                        <Loader loading={true} />
+                    ) :
+                        <>
+
+                            <div className="content__container">
+                                <div className="rights__container">
+                                    {rights.length === 0 ? (
+                                        <p>No rights sets found for this service.</p>
+                                    ) : (
+                                        <ServiceRightsComponent rights={rights} rightsDetailsLimit={rightsDetailsLimit} />
+                                    )}
                                 </div>
-                            )
-                        })
-                    )
+                            </div>
+                        </>
                 }
             </div>
         </div>
