fa25e77f510e9d78bbef0bbd173e687646a7cef7
naflouille <eadjjawn@gmail.com>
2025-10-13 09:14:57 +0200

    UI Enhancements to the docs

--- frontend/src/pages/docs/page.tsx
+++ frontend/src/pages/docs/page.tsx
@@ -62,19 +62,19 @@ const DocsPageDoc = () => {
             <div className="docs-page">
                 <div className="docs-page-welcome">
                     <div className="docs__page__header">
-                    <div className="header__head">
-                        <img
-                            src="https://naflows.com/public/assets/naflows_full_logotype.png"
-                            alt="Naflows Full Logotype"
-                            style={{ cursor: 'pointer' }}
-                            onClick={(() => { window.location.href = '/docs' })}
-                        />
-                        <button className='secondary-button' onClick={() => {
-                            window.location.href = "https://app.naflows.com";
-                        }}>
-                            Open NASS App
-                        </button>
-                    </div>
+                        <div className="header__head">
+                            <img
+                                src="https://naflows.com/public/assets/naflows_full_logotype.png"
+                                alt="Naflows Full Logotype"
+                                style={{ cursor: 'pointer' }}
+                                onClick={(() => { window.location.href = '/docs' })}
+                            />
+                            <button className='secondary-button' onClick={() => {
+                                window.location.href = "https://app.naflows.com";
+                            }}>
+                                Open NASS App
+                            </button>
+                        </div>
                         <div className="docs__page__title">
                             <h3><span className="category" onClick={() => {
                                 window.location.href = `/docs/${dir}`;
@@ -131,32 +131,35 @@ const DocsPageDoc = () => {
                                                 </div>
                                             ))}
                                         </div>
-                                    <div className="docs__page__references docs__page__section">
-                                        <h5 className="docs__page__section__title">References</h5>
-                                        <div className="references__content">
-                                            {pageContent && (pageContent as any).references && (pageContent as any).references.map((ref: any, index: number) => (
-                                                <div className="reference" key={index} onClick={() => {
-                                                    window.open(ref.link, "_blank");
-                                                }}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h240q17 0 28.5 11.5T480-800q0 17-11.5 28.5T440-760H200v560h560v-240q0-17 11.5-28.5T800-480q17 0 28.5 11.5T840-440v240q0 33-23.5 56.5T760-120H200Zm560-584L416-360q-11 11-28 11t-28-11q-11-11-11-28t11-28l344-344H600q-17 0-28.5-11.5T560-800q0-17 11.5-28.5T600-840h200q17 0 28.5 11.5T840-800v200q0 17-11.5 28.5T800-560q-17 0-28.5-11.5T760-600v-104Z" /></svg>
-                                                    <span>{ref.text}</span>
-                                                </div>
-                                            ))}
+                                        <div className="docs__page__references docs__page__section">
+                                            <h5 className="docs__page__section__title">References</h5>
+                                            <div className="references__content">
+                                                {pageContent && (pageContent as any).references && (pageContent as any).references.map((ref: any, index: number) => (
+                                                    <div className="reference" key={index} onClick={() => {
+                                                        window.open(ref.link, "_blank");
+                                                    }}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h240q17 0 28.5 11.5T480-800q0 17-11.5 28.5T440-760H200v560h560v-240q0-17 11.5-28.5T800-480q17 0 28.5 11.5T840-440v240q0 33-23.5 56.5T760-120H200Zm560-584L416-360q-11 11-28 11t-28-11q-11-11-11-28t11-28l344-344H600q-17 0-28.5-11.5T560-800q0-17 11.5-28.5T600-840h200q17 0 28.5 11.5T840-800v200q0 17-11.5 28.5T800-560q-17 0-28.5-11.5T760-600v-104Z" /></svg>
+                                                        <span>{ref.text}</span>
+                                                    </div>
+                                                ))}
+                                            </div>
                                         </div>
-                                    </div>
                                     </div>
 
 
                                 </>
                             ) : (
                                 <div className="docs__page__content">
-                                    <h4>Please select a page from the directory.</h4>
                                     <div className="directory">
                                         {content.content.map((page: any) => (
                                             <div key={page.id} className="directory__item" onClick={() => {
                                                 window.location.href = `/docs/${dir}/${page.page}`;
                                             }}>
-                                                {page.title}
+                                                <div className="directory__item__body">
+                                                    <h5 className="directory__item__title">{page.title}</h5>
+                                                    <span className="directory__item__description">{page.description}</span>
+                                                </div>
+                                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M504-480 348-636q-11-11-11-28t11-28q11-11 28-11t28 11l184 184q6 6 8.5 13t2.5 15q0 8-2.5 15t-8.5 13L404-268q-11 11-28 11t-28-11q-11-11-11-28t11-28l156-156Z" /></svg>
                                             </div>
                                         ))}
                                     </div>
