0b6e82b5cc2714536b1a0ee3578c416b2c0c4233
naflouille <eadjjawn@gmail.com>
2025-11-04 08:10:07 +0100

    feat: refactor database reset logic in run.sh and update file permissions

--- run.sh
+++ run.sh
@@ -5,6 +5,21 @@ RESET_ENVIRONMENT=$1
 RESET_DB=$2
 
 
+function reset_db {
+    echo -e "\033[1;32mResetting database...\033[0m"
+    COMPOSE_PROFILES="mongo-nass" docker compose down -v
+    echo -e "\033[1;32mStarting MongoDB...\033[0m"
+    COMPOSE_PROFILES="mongo-nass,auth-api,mongo-express" docker compose up -d --build
+    # Wait for MongoDB to be ready
+    sleep 5
+
+    # Initialize database manually
+    echo -e "\033[1;32mInitializing database...\033[0m"
+    docker exec -i mongo-nass mongosh -u XQaLCRrQWDir -p vGaHbhFzAOrq --authenticationDatabase admin < ./mongo-init/init.js
+
+}
+
+
 if [ "$RESET_ENVIRONMENT" = "true" ]; then
     echo -e "\033[1;32mCleaning up existing Docker containers and volumes...\033[0m"
     rm -rf ./backend/auth-data
@@ -21,18 +36,7 @@ else
 fi
 
 if [ "$RESET_DB" = "true" ]; then
-    echo -e "\033[1;32mResetting database...\033[0m"
-    COMPOSE_PROFILES="mongo-nass" docker compose down -v
-    echo -e "\033[1;32mStarting MongoDB...\033[0m"
-    COMPOSE_PROFILES="mongo-nass" docker compose up -d mongo-nass
-    # Wait for MongoDB to be ready
-    sleep 5
-
-    # Initialize database manually
-    echo -e "\033[1;32mInitializing database...\033[0m"
-    docker exec -i mongo-nass mongosh -u admin -p secret --authenticationDatabase admin < ./mongo-init/init.js
-
-    echo -e "\033[1;32mStarting other services...\033[0m"
+    reset_db
 fi
 
 
